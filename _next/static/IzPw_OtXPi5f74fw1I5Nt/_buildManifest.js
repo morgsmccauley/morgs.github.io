ec53a3787c5779ff23e3f48e3d89630fa5cc953d
morgsmccauley <morgan@mccauley.co.nz>
2021-02-06 23:20:23 +0000

    Deploying to gh-pages from  @ c1c34acb60bc5028e71393365ad2d4018005eabd 🚀

--- _next/static/IzPw_OtXPi5f74fw1I5Nt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-1438c1bc9025ace6d463.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-951d99eeaa3cc06d8231.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
